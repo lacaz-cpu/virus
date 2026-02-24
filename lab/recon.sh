73e8cb90141140a28255720927d1cb0ff938f23e
lacaz-cpu <arthur.obry.2n16@gmail.com>
2026-02-24 23:27:58 +0100

    recon

--- lab/recon.sh
+++ lab/recon.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-find "$1" -type f | while read -r file; do
+if [ -e "$1" ]; then
+    echo "Analyse de $1 : "  
+find "$1" -type f \( -name '*.dat' -o -name '*.txt' \) | while read -r file; do
     echo "Chemin $file"
-    echo "taille $(du -h "$file" | cut -f1)"
-    echo "date de modification $(stat -c %y "$file") \n"
+    echo "taille $(du -b "$file" | cut -f1) octets"
+    echo "date de modification $(stat -c %y "$file") "
 done
+
+else
+    echo "Chemin '$1' inexistant"
+    exit 1
+fi 
