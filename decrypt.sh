dc0f94bd57fc844088c057fba4692452724bad60
lacaz-cpu <arthur.obry.2n16@gmail.com>
2026-03-08 03:33:53 +0100

    TP fini

--- decrypt.sh
+++ decrypt.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 source xor_helper.sh
+decrypt_file() {
+    if [ -f .cryptoshell_key ]; then
+        key=$(<.cryptoshell_key)
+        xor_file "$1" "${1%.locked}" "$key"
+        rm -f "$1"
+    else
+        echo "Aucun fichier .cryptoshell_key trouvé. Impossible de décrypter $1."
+    fi
+}
 
-key=$(<.cryptoshell_key)
-xor_file "$1" "${1%.locked}" "$key"
-rm -f "$1"
+#key=$(<.cryptoshell_key)
+#xor_file "$1" "${1%.locked}" "$key"
+#rm -f "$1"
--- recovery.sh
+++ recovery.sh
@@ -6,8 +6,10 @@ if [ -d "$1" ]; then
     if [ $(find "$1" -type f -name ".cryptoshell_key" | wc -l) -gt 0 ]; then
         find "$1" -type f -name "*.locked" | while read -r file; do
             decrypt_file "$file"
+            
         done
         rm -f "$1/.cryptoshell_key"
+        find "$1" -type f -name "ransom_note.txt" -delete
         exit 1
     else
         echo "Aucun fichier .cryptoshell_key trouvé dans le dossier spécifié."
