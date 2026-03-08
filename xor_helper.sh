d89dbc2b0349437c3eec8dc8486cddb191e6762a
lacaz-cpu <arthur.obry.2n16@gmail.com>
2026-03-08 03:52:05 +0100

    correction final

--- xor_helper.sh
+++ xor_helper.sh
@@ -14,7 +14,9 @@ xor_file() {
     local input="$1" output="$2" key="$3"
     local key_len=${#key}
     local i=0
-    xxd -p "$input" | tr -d '\n' | fold -w2 | while read byte; do
+    #echo "\n" >> "$input" 2>/dev/null mauvaise facon de corriger l'erreur de supression du dernier octet
+    xxd -p "$input" | tr -d '\n' | fold -w2 | while read byte || [ -n "$byte" ]; do # permet de vérifier la dernière
+    # qui lorsque executer avec un simple read ne rentre pas dans la boucle car read retourne une erreur -n $byte permet donc de corriger cela
         local ki=$(( i % (key_len / 2) ))
         local key_byte="${key:$((ki*2)):2}"
         printf "%02x" $(( 16#$byte ^ 16#$key_byte ))
